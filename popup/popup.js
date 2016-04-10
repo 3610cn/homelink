165a1c7ff274da9098984b48be1568c09e37e0d9
maoquan <maoquan0515@163.com>
2016-04-10 09:54:40 +0800

    插上缺失日期

--- popup/popup.js
+++ popup/popup.js
@@ -16,6 +16,9 @@ define(
         var m = require('moment');
         var echarts = require('echarts');
 
+        // 链接接口中跟时间相关的格式
+        var TIME_FORMAT = 'YYYY-MM-DD';
+
         function showStatus(message) {
             $('#status').html(message).show();
         }
@@ -191,7 +194,7 @@ define(
             records.forEach(
                 function (record) {
                     record = m(record.see_time || record.lookTimeString, 'YYYY-MM-DD hh:mm:ss');
-                    record = record.format('YYYY-MM-DD');
+                    record = record.format(TIME_FORMAT);
                     if (merged[record] > 0) {
                         merged[record]++;
                     }
@@ -204,22 +207,55 @@ define(
             util.each(
                 merged,
                 function (value, key) {
-                    key = m(key, 'YYYY-MM-DD');
+                    key = m(key, TIME_FORMAT);
                     newRecords.push(
                         {
-                            time: key.format('YYYY-MM-DD'),
+                            time: key.format(TIME_FORMAT),
                             week: [null, '一', '二', '三', '四', '五', '六', '日'][key.isoWeekday()],
                             count: value
                         }
                     );
                 }
             );
-            newRecords.sort(
-                function (a, b) {
-                    return m(a.time, 'YYYY-MM-DD')
-                        .isBefore(
-                            m(b.time, 'YYYY-MM-DD')
-                        ) ? -1 : 1;
+            // 根据时间字段排序
+            var timeSorter = function (a, b) {
+                return m(a.time, TIME_FORMAT).isBefore(
+                    m(b.time, TIME_FORMAT)
+                ) ? -1 : 1;
+            };
+
+            newRecords.sort(timeSorter);
+
+            // 这一段逻辑是为没有的日期补上数据，count默认为0
+            var current = newRecords[0].time;
+            current = m(current, TIME_FORMAT);
+            var end = newRecords[newRecords.length - 1].time;
+            end = m(end, TIME_FORMAT);
+            var toAdd = [];
+            while (true) {
+                current.add(1, 'd');
+                if (current.isSameOrAfter(end)) {
+                    break;
+                }
+                var currentTime = current.format(TIME_FORMAT);
+                if (!merged[currentTime]) {
+                    toAdd.push(
+                        {
+                            time: currentTime,
+                            count: 0
+                        }
+                    );
+                }
+            }
+
+            var chartRecords = newRecords.concat(toAdd).sort(timeSorter);
+            // 过滤2016-03-01之前的数据
+            chartRecords = chartRecords.filter(
+                function (record) {
+                    if (m(record.time, TIME_FORMAT).isBefore(m('2016-03-01', TIME_FORMAT))) {
+                        return false;
+                    }
+                    return true;
                 }
             );
 
@@ -238,13 +274,13 @@ define(
                     data:['看房记录']
                 },
                 xAxis: {
-                    data: util.pluck(newRecords, 'time')
+                    data: util.pluck(chartRecords, 'time')
                 },
                 yAxis: {},
                 series: [{
                     name: '看房量',
                     type: 'line',
-                    data: util.pluck(newRecords, 'count')
+                    data: util.pluck(chartRecords, 'count')
                 }]
             };
             recordChart.showLoading({text: 'Loading'});
